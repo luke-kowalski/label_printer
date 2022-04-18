6ccade784ea670ecce0e0abc1f96bd9ace028144
LK <lukasz.fialki@gmail.com>
2022-04-18 16:08:43 +0200

    render pdf added

---
+++ label_creator.py
@@ -0,0 +1,62 @@
+import pyodbc
+import settings
+import os
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.units import mm
+import win32api
+import win32print
+
+
+conn_STRING = "DRIVER={};SERVER={};DATABASE={};UID={};PWD={}".format(
+    settings.sql_driver,
+    settings.sql_server,
+    settings.sql_database,
+    settings.sql_username,
+    settings.sql_password,
+)
+
+
+def get_user_input():
+    print("Podaj nazwę zlecenia: ")
+    order_id = str(input())
+    get_sql_data(order_id)
+
+
+def get_sql_data(order_id):
+
+    try:
+        with pyodbc.connect(conn_STRING) as conn:
+            rows = conn.execute(
+                "SELECT * FROM PROADMIN WHERE NAME = ?", (order_id)
+            ).fetchall()
+    except pyodbc.Error as e:
+        sqlstate = e.args[1]
+        print(f"ERROR: {sqlstate}")
+    else:
+        render_pdf(rows)
+
+
+def render_pdf(rows):
+
+    path = settings.html_output_dir
+    isExist = os.path.exists(path)
+
+    if not isExist:
+        os.makedirs(path)
+        print(f"Directory has been created: {path}")
+
+    for row in rows:
+        filename = path + row.NAME + ".pdf"
+
+        canvas = Canvas(filename, pagesize=(100 * mm, 60 * mm))
+        canvas.setFont("Times-Roman", 10)
+        canvas.drawString(5, 50, f"{row.NAME} - {row.ID}")
+        canvas.save()
+
+        win32api.ShellExecute(
+            0, "printto", filename, '"%s"' % win32print.GetDefaultPrinter(), ".", 0
+        )
+
+
+while True:
+    get_user_input()
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
-from configparser import ConfigParser
+from configparser import ConfigParser, ExtendedInterpolation
 
-parser = ConfigParser()
+parser = ConfigParser(interpolation=ExtendedInterpolation())
+parser.optionxform = str
 parser.read("config.ini")
 
 sql_driver = parser.get("SERVER_CONN", "DRIVER")
@@ -8,5 +9,8 @@ sql_server = parser.get("SERVER_CONN", "SERVER")
 sql_database = parser.get("SERVER_CONN", "DATABASE")
 sql_username = parser.get("SERVER_CONN", "USERNAME")
 sql_password = parser.get("SERVER_CONN", "PASSWORD")
-
 sql_query = parser.get("SQL", "SQL_QUERRY")
+# DIR
+html_output_dir = parser.get("DIR", "HTML_OUTPUT_DIR")
+
+
